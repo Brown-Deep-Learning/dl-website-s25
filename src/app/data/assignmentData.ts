1e6d797f3a9caa0c18303e9ec7103c000c3b94a4
Dave Lubawski <77742798+DingerDave@users.noreply.github.com>
2025-01-29 12:14:33 -0500

    Update assignmentData.ts

--- src/app/data/assignmentData.ts
+++ src/app/data/assignmentData.ts
@@ -15,5 +15,16 @@ export const assignments = [
       link: "https://hackmd.io/@BDLS25/S1dv0oLDkx",
     },
   },
-  // Add more assignments as needed
+  {
+    id: 2,
+    name: "Mini-Project 1: Deep Learning with Tensorflow and Optimizers",
+    link: "https://colab.research.google.com/drive/1G-REdGzp-pyxlqXSXOftmPL3iRO5Rt2x#scrollTo=dB-a3rs6gpa1",
+    inDate: "Wednesday, January 29th",
+    outDate: "Wednesday, Febuary 5th",
+    programming: {
+      title: "Stencil Notebook",
+      link: "https://colab.research.google.com/drive/1G-REdGzp-pyxlqXSXOftmPL3iRO5Rt2x#scrollTo=dB-a3rs6gpa1",
+    },
+  },
+    
 ];
