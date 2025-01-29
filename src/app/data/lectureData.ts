4f6adf38930c53993f01566d9347669b83a01674
Eric-Ewing <43459843+Eric-Ewing@users.noreply.github.com>
2025-01-29 16:31:52 -0500

    Update lectureData.ts

--- src/app/data/lectureData.ts
+++ src/app/data/lectureData.ts
@@ -40,7 +40,7 @@ export const lectureGroups: LectureGroup[] = [
       {
         id: 5,
         title: "Loss Functions and Gradient Descent",
-        date: "2025-01-29",
+        date: "2025-02-3",
         // slidesLink: "slides/lecture4.pdf",
         // recordingLink:
         // "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=d8fb8f55-a37e-4ad8-b161-b26c0177a70d",
@@ -48,7 +48,7 @@ export const lectureGroups: LectureGroup[] = [
       {
         id: 6,
         title: "Backpropagation",
-        date: "2025-01-29",
+        date: "2025-02-5",
         // slidesLink: "slides/lecture4.pdf",
         // recordingLink:
         // "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=d8fb8f55-a37e-4ad8-b161-b26c0177a70d",
@@ -56,7 +56,7 @@ export const lectureGroups: LectureGroup[] = [
       {
         id: 7,
         title: "Tensorflow and Autodiff",
-        date: "2025-01-29",
+        date: "2025-02-7",
         // slidesLink: "slides/lecture4.pdf",
         // recordingLink:
         // "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=d8fb8f55-a37e-4ad8-b161-b26c0177a70d",
@@ -64,7 +64,7 @@ export const lectureGroups: LectureGroup[] = [
             {
         id: 8,
         title: "Linear Algebra, Matrices, and GPUs",
-        date: "2025-01-29",
+        date: "2025-02-10",
         // slidesLink: "slides/lecture4.pdf",
         // recordingLink:
         // "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=d8fb8f55-a37e-4ad8-b161-b26c0177a70d",
@@ -72,7 +72,7 @@ export const lectureGroups: LectureGroup[] = [
       {
         id: 9,
         title: "Building Blocks of Deep Learning: Activation functions and multi-layer networks",
-        date: "2025-01-29",
+        date: "2025-02-12",
         // slidesLink: "slides/lecture4.pdf",
         // recordingLink:
         // "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=d8fb8f55-a37e-4ad8-b161-b26c0177a70d",
@@ -80,7 +80,7 @@ export const lectureGroups: LectureGroup[] = [
       {
         id: 10,
         title: "Hyperparameter Tuning and Practical Advice for Training",
-        date: "2025-01-29",
+        date: "2025-02-14",
         // slidesLink: "slides/lecture4.pdf",
         // recordingLink:
         // "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=d8fb8f55-a37e-4ad8-b161-b26c0177a70d",
@@ -88,7 +88,7 @@ export const lectureGroups: LectureGroup[] = [
       {
         id: 10,
         title: "Convolutions Day 1",
-        date: "2025-01-29",
+        date: "2025-02-19",
         // slidesLink: "slides/lecture4.pdf",
         // recordingLink:
         // "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=d8fb8f55-a37e-4ad8-b161-b26c0177a70d",
