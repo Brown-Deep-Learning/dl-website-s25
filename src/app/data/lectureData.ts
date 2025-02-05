a552ad5b0bcc5f877f05d98dbbf203cedc60ef91
Dave Lubawski <77742798+DingerDave@users.noreply.github.com>
2025-02-05 12:14:47 -0500

    Update lectureData.ts

--- src/app/data/lectureData.ts
+++ src/app/data/lectureData.ts
@@ -50,8 +50,8 @@ export const lectureGroups: LectureGroup[] = [
         title: "Backpropagation and SGD",
         date: "2025-02-3",
         slidesLink: "slides/Lecture6.pdf",
-        // recordingLink:
-        // "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=d8fb8f55-a37e-4ad8-b161-b26c0177a70d",
+        recordingLink:
+        "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=33b77d05-52d7-4f5f-a005-b26c0177a750",
       },
       {
         id: 7,
