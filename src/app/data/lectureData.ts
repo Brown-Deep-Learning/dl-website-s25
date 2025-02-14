fd3e46d3d00d1e69cd0a996e856f284a373815e0
Eric-Ewing <43459843+Eric-Ewing@users.noreply.github.com>
2025-02-14 11:24:43 -0500

    Update lectureData.ts

--- src/app/data/lectureData.ts
+++ src/app/data/lectureData.ts
@@ -85,6 +85,14 @@ export const lectureGroups: LectureGroup[] = [
         // recordingLink:
         // "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=d8fb8f55-a37e-4ad8-b161-b26c0177a70d",
       },
+            {
+        id: 10,
+        title: "Convolutions Day 2",
+        date: "2025-02-14",
+        slidesLink: "slides/lecture11.pdf",
+        // recordingLink:
+        // "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=d8fb8f55-a37e-4ad8-b161-b26c0177a70d",
+      },
     ],
   },
   // Add more groups for Weeks 5-8, 9-12, etc. as necessary
