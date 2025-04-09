4a374807b6732b11cf4a9296627ee9555f45304e
Eric-Ewing <43459843+Eric-Ewing@users.noreply.github.com>
2025-04-09 11:50:28 -0400

    Update lectureData.ts

--- src/app/data/lectureData.ts
+++ src/app/data/lectureData.ts
@@ -237,6 +237,14 @@ export const lectureGroups: LectureGroup[] = [
         // recordingLink:
         //   "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=6a9fab3a-15fd-4b1d-91b1-b26c0177aa70",
       },
+            {
+        id: 24,
+        title: "Conditional Generative Models and Intro to RL",
+        date: "2025-04-9",
+        slidesLink: "slides/lecture29.pdf",
+        // recordingLink:
+        //   "https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=6a9fab3a-15fd-4b1d-91b1-b26c0177aa70",
+      },
     ],
   },
 ];
